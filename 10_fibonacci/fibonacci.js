ea4de6421e0addbbddba782e174dd679e953a48d
pkhromchenko <pavelkhrcs@gmail.com>
2022-11-20 15:23:16 -0500

    fibo complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (nthValue) {
+  if (nthValue < 0) {
+    return "OOPS";
+  }
+  var fib = [0, 1];
+  for (let i = 2; i < 30; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
 
+  return fib[parseInt(nthValue)];
 };
 
 // Do not edit below this line
